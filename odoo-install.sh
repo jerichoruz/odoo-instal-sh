af7fe42c4374fcedc46fa96fc8d01a7b11955873
Jericho Ruz <potosho@gmail.com>
2020-10-07 15:35:42 -0500

    Update odoo-install.sh

--- odoo-install.sh
+++ odoo-install.sh
@@ -61,9 +61,12 @@ echo -e "\n---- Update Server ----"
 # universe package is for Ubuntu 18.x
 sudo add-apt-repository universe
 # libpng12-0 dependency for wkhtmltopdf
+sudo add-apt-repository ppa:linuxuprising/libpng12
 sudo add-apt-repository "deb http://mirrors.kernel.org/ubuntu/ xenial main"
 sudo apt-get update
+sudo apt install libpng12-0
 sudo apt-get upgrade -y
+
 
 #--------------------------------------------------
 # Install PostgreSQL Server
